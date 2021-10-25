5856a47f4984cd201673c4f8d4701ef2eb028021
Clay <claybattle12@gmail.com>
2021-10-25 19:31:38 -0400

    Added Clay Battle to main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 def main():
     pygame.init()
-    team = {"lead": "Senih O", "backend": "Sharan S", "frontend": "Clay B"}
+    team = {"lead": "Senih O", "backend": "Sharan S", "frontend": "Clay Battle"}
     print("Software Lead is:", team["lead"])
     print("Backend is:", team["backend"])
     print("Frontend is:" , team["frontend"])
