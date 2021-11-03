441d41d33b78e2107923e613e8b8208508a4e7eb
Senih Okuyucu <senihokuyucu@Senihs-MacBook-Air.local>
2021-11-03 16:48:06 -0400

    I hope this figures itself out

--- apiCall.py
+++ apiCall.py
@@ -2,27 +2,16 @@
 #Notes: tweepy import won't work , no clue why
 # you can access apiCodes.py by unzipping the Codes.zip file, ask me for the password
 
-# import tweepy
-
-
-
+import tweepy
 from assets import apiCodes
-tw = "tweepy"
+
 # api_key = apiCodes.Codes().assignapi_key
 # api_key_scrt = apiCodes.Codes().api_key_scrt
 # access_tkn = apiCodes.Codes().access_tkn
 # access_tkn_scrt = apiCodes.Codes().access_tkn_scrt
-print(apiCodes.Codes().assignValues())
+# print(apiCodes.Codes().assignValues())
 
-"""
-### Hey guys my secret auth tokens supposed to be here but I will not be pushing them with the repo ###
-auth = tw.OAuthHandler(api_key, api_key_scrt)
-auth.set_access_token(access_tkn, access_tkn_scrt)
 
-api = tw.API(auth)
-
-us_woeid = 23424977
-national_trends = api.get_place_trends(id, us_woeid)
 
 def getTrends():
     trends = []
@@ -30,15 +19,22 @@ def getTrends():
         if type(trend["tweet_volume"]) == int:
             trends.append((trend["name"], trend["tweet_volume"]))
     trends.append(len(trends))
+    print(trends)
     return trends
 
 def main():
+    codes = apiCodes.Codes().assignValues()
+    auth = tweepy.OAuthHandler(codes["api_key"], codes["api_key_scrt"])
+    auth.set_access_token(codes["access_tkn"], codes["access_tkn_scrt"])
+    api = tweepy.API(auth)
+    us_woeid = 23424977
+    national_trends = api.get_place_trends(id, 23424977)
     deck = getTrends()
     print(type(deck))
-    print(deck)
+    # print(deck)
 
     return deck
 
 main()
-"""
+
 
