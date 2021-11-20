d5d1cb1f92965d7db264976f69b1efb639b81285
Sharan-Saha <71471687+Sharan-Saha@users.noreply.github.com>
2021-11-19 21:32:24 -0500

    commit

--- src/button.py
+++ src/button.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, name, x, y, img_file):
+    def __init__(self, name, x, y, img_file, text):
 
         pygame.sprite.Sprite.__init__(self)
 
@@ -10,3 +10,15 @@ class Button(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y 
+        self.text = text
+    
+    def hover(self, mouse_x, mouse_y):
+        #not sure if this should be in this class or in the controller class
+        return True
+
+    def update(self, new_text):
+        """
+            updates the old text over the button to the new text, which in this case would be the names of the trends the user will be guessing between 
+            parameters: self, self.text new_text
+        """
+        self.text = new_text
