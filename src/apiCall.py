ea36522257a5e1c5b507de5f9a72dfa5528a4dc7
Sharan-Saha <71471687+Sharan-Saha@users.noreply.github.com>
2021-12-01 22:27:25 -0500

    Added docstrings to apicall

--- src/apiCall.py
+++ src/apiCall.py
@@ -7,22 +7,37 @@ from pathlib import Path
 # you can access apiCodes.py by unzipping the Codes.zip file, ask me for the password
 
 class ApiCall:
-    def __init__(self):
 
+    
+    def __init__(self):
+        """
+        Initializes codes, auth, access_token, and api 
+        Parameters: self
+        Returns:none
+        """
         self.codes = apiCodes.Codes().assignValues()
         self.auth = tweepy.OAuthHandler(self.codes["api_key"], self.codes["api_key_scrt"])
         self.auth.set_access_token(self.codes["access_tkn"], self.codes["access_tkn_scrt"])
         self.api = tweepy.API(self.auth)
 
-        
+      
     def setApi(self):
-        
+        """
+        Gets and returns the national trends with and without tweet counts
+        Parameters: self
+        Returns: self.national_trends
+        """
         self.us_woeid = 23424977
         self.national_trends = self.api.get_place_trends(self.us_woeid)
 
         return self.national_trends
     
     def callApi(self):
+        """
+        Initializes a trends list and for each item that has a corresponding tweet in national_trends, appends it to trends
+        Parameters: self
+        Returns: self.trends 
+        """
         self.national_trends = self.setApi()
         self.trends = []
         for trend in self.national_trends[0]["trends"][:50]:
@@ -33,8 +48,14 @@ class ApiCall:
         print(self.trends)
 
         return self.trends
-    
+        
     def getTrends(self):
+        """
+        Checks to see if trends.json exists. If it does, it reads the data in the file and if that data is longer than 15 minutes it updates the items in the trends list and dumps it in the json file. If the file doesn't exist, it creates a trends.json file and and dumps the most up to date trends list into the newly created file
+        Parameters: self
+        Returns: none
+
+        """
         self.path = Path('trends.json')
         if self.path.is_file():
             with open(self.path) as readfile:
@@ -47,9 +68,6 @@ class ApiCall:
         with open(self.path, "w") as outfile:
             json.dump(self.trends, outfile)
 
-a = ApiCall()
-
-a.getTrends()
 
 # def callApi():
 #     codes = apiCodes.Codes().assignValues()
